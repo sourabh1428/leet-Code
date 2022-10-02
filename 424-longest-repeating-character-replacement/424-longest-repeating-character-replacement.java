5326b6c5a42723a6740a1bcc809c7763b88fd89b
Sourabh Pathak <63462210+sourabh1428@users.noreply.github.com>
2022-10-02 21:03:08 +0530

    Time: 10 ms (69.43%), Space: 43.4 MB (38.93%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -1,25 +1,19 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        // Make an array of size 26...
-        int[] arr = new int[26];
-        // Initialize largestCount, maxlen & beg pointer...
-        int largestCount = 0, beg = 0, maxlen = 0;
-        // Traverse all characters through the loop...
-        for(int end = 0; end < s.length(); end ++){
-            arr[s.charAt(end) - 'A']++;
-            // Get the largest count of a single, unique character in the current window...
-            largestCount = Math.max(largestCount, arr[s.charAt(end) - 'A']);
-            // We are allowed to have at most k replacements in the window...
-            // So, if max character frequency + distance between beg and end is greater than k...
-            // this means we have considered changing more than k charactres. So time to shrink window...
-            // Then there are more characters in the window than we can replace, and we need to shrink the window...
-            if(end - beg + 1 - largestCount > k){     // The main equation is: end - beg + 1 - largestCount...
-                arr[s.charAt(beg) - 'A']--;
-                beg ++;
+        
+        int[] table= new int[26];
+        int result=0;
+        int maxF=0;
+        for(int r=0,l=0;r<s.length();r++) {
+            table[s.charAt(r)-'A']+=1;
+            maxF=Math.max(table[s.charAt(r)-'A'],maxF);
+            if((r-l+1)-maxF>k) {
+                table[s.charAt(l)-'A']-=1;
+                l++;
             }
-            // Get the maximum length of repeating character...
-            maxlen = Math.max(maxlen, end - beg + 1);     // end - beg + 1 = size of the current window...
+            result=Math.max(result,r-l+1);
         }
-        return maxlen;      // Return the maximum length of repeating character...
+        return result;
     }
-}
+        
+    }
