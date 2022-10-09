ef4d376002561a9cb055c080c003bb2b66efe986
Sourabh Pathak <63462210+sourabh1428@users.noreply.github.com>
2022-10-09 20:49:42 +0530

    Time: 25 ms (52.65%), Space: 44.7 MB (54.83%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -0,0 +1,56 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        
+        
+        if(s.equals(t)) {
+            return s;
+        }
+        
+        HashMap<Character, Integer> pattMap = new HashMap<>();
+        
+        int i=0, j=0;
+        int m=0, n=0;
+        int max = s.length();
+        
+        for(int k = 0; k < t.length(); k++) {
+            pattMap.put(t.charAt(k), pattMap.getOrDefault(t.charAt(k), 0)+1);
+        }
+        
+        int count = pattMap.size();
+        
+        while(i <= j) {
+            
+            if(count > 0) {
+                if(j >= s.length()) {
+                    break;
+                }
+                if(pattMap.containsKey(s.charAt(j))) {
+                    pattMap.put(s.charAt(j), pattMap.get(s.charAt(j))-1);
+                    if(pattMap.get(s.charAt(j)) == 0) {
+                        count--;
+                    }
+                }
+                
+                j++;
+            } else {
+                
+                if((j-i) <= max) {
+                    m = i;
+                    n = j;
+                    max = j-i;
+                }
+                
+                if(pattMap.containsKey(s.charAt(i))) {
+                    if(pattMap.get(s.charAt(i)) == 0) {
+                        count++;
+                    }
+                    pattMap.put(s.charAt(i), pattMap.get(s.charAt(i))+1);
+                }
+                
+                i++;
+            }
+        }
+        
+        return s.substring(m, n);
+    }
+}
