ea0fa482aa6919ea7716afecf48e3bbcb91d5040
Sourabh Pathak <63462210+sourabh1428@users.noreply.github.com>
2022-10-10 21:29:11 +0530

    Time: 5 ms (82.10%), Space: 43.6 MB (98.95%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,9 +8,19 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+
+
+//overall concept isme ye use hua hai ki bss pehle listnode se pooore values ko priority queue me dalenge (min priority queue) to apne aap sort hote jauyega
+
+
+
+
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+       //priority queue me dalenege pooore vlaue ko listnode se 
         
         for(ListNode list : lists)
         {
@@ -21,8 +31,18 @@ class Solution {
             }
         }
         
+        
+        
+        
         ListNode ans = new ListNode(0);
         if(pq.size()==0) return null;
+        
+        
+        
+        //ab priority queue se listnode me dalenge 
+        
+        
+        
         else{
             ListNode temp = ans;
             while(pq.size()!=0){
