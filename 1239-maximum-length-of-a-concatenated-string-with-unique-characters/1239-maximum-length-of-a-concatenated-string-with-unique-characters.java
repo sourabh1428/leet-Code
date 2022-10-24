62e7e24f0fc11d1ce84a8bdfdc506b8c7ea5fe38
Sourabh Pathak <63462210+sourabh1428@users.noreply.github.com>
2022-10-24 11:56:12 +0530

    Time: 48 ms (50.12%), Space: 54.9 MB (42.10%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,40 +1,40 @@
 class Solution {
-    int ans = 0;
     public int maxLength(List<String> arr) {
-        HashSet<Character> hs = new HashSet<>();    // storing unique characters in it
-        helper(arr, 0, 0, hs);
-        return ans;
-    }
-    
-    public int helper(List<String> arr, int idx, int count, HashSet<Character> hs){  // count variable store the length of unique concatenated string
-        if(idx == arr.size()){
-            return count;
-        }
-        for(int i = idx; i < arr.size(); i++){
-            boolean flag = true;
-            HashSet<Character> temp = new HashSet<>();
-            for(int j = 0; j < arr.get(i).length(); j++){
-                if(hs.contains(arr.get(i).charAt(j))){                 // if the ith string has any character which occured before in previous string then made flag false and break the loop
-                    flag = false;
-                    break;
-                }
-                temp.add(arr.get(i).charAt(j));              // here we are adding all characters in different hashset to check that any character in ith string occured 2 or more times or not.
-                
-            }
-            if(flag == true && temp.size() == arr.get(i).length()){   // if ith string does not contains any character which occured before and in ith string also does not have any character which occured 2 or more times.
-                for(int j = 0; j < arr.get(i).length(); j++){
-                    hs.add(arr.get(i).charAt(j));
-                }
-                ans = Math.max(ans, helper(arr, i + 1, count + arr.get(i).length(), hs));  // if ith string  does not contains any same character then add the length of ith string in count and move to the next string 
-                for(int j = 0; j < arr.get(i).length(); j++){
-                    hs.remove(arr.get(i).charAt(j));      // in back tracking remove those characters which we have added before
-                }
-            }else{
-                ans = Math.max(ans, helper(arr, i + 1, count, hs));    // if ith string containing any same character then we move on to the next index without including the ith string.
-            }
-            
-        }
-        return ans;   // return maximum concatenated unique character string
+        // edge case
+        if(arr == null || arr.size() == 0) return 0;
         
+        // inintialiazation
+        Set<Character> chars = new HashSet();
+        return maxLength(arr, 0, chars);
+    }
+    public int maxLength(List<String> arr, int index, Set<Character> chars){
+        // edge case
+        if(index >= arr.size()) return 0;
+        
+        // without considering the current index string
+        int currMax = maxLength(arr, index + 1, chars);
+        
+        // When we consider the current string
+        Set<Character> currChars = new HashSet();
+        boolean isUnique = true;
+        
+        // add the chars into a set and check if
+        // chars are unique locally and globally
+        
+        for(char ch : arr.get(index).toCharArray()){
+            if(chars.contains(ch) || currChars.contains(ch)){
+                isUnique = false;
+                break;
+            }
+            currChars.add(ch);
+        }
+        // when characters of current string is unique
+        if(isUnique){
+            chars.addAll(currChars);
+            currMax = Math.max(currMax, arr.get(index).length()+maxLength(arr, index+1, chars));
+            chars.removeAll(currChars);
+                
+        }
+        return currMax;
     }
 }
