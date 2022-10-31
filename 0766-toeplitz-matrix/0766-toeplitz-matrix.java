50833de9ad0d892954df2608dcb8a409335308a4
Sourabh Pathak <63462210+sourabh1428@users.noreply.github.com>
2022-10-31 11:03:50 +0530

    Time: 1 ms (93.83%), Space: 46.4 MB (11.91%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,16 +1,10 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        if(matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) return true;
-        int row = matrix.length;
-        int col = matrix[0].length;
-        List<Integer> buffer = new LinkedList<>();
-        for(int j = 0; j < col; j++) buffer.add(matrix[0][j]);
-        for(int i = 1; i < row; i++){
-            for(int j = 1; j < col; j++){
-                if(buffer.get(j - 1) != matrix[i][j]) return false;
+      for(int i=0; i<matrix.length-1; i++){
+            for(int j=0; j<matrix[0].length-1; j++){
+                if(matrix[i][j] != matrix[i+1][j+1])
+                    return false;
             }
-            buffer.remove(buffer.size() - 1);
-            buffer.add(0, matrix[i][0]);
         }
         return true;
     }
