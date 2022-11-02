1582d24ab6cec777d342f9937b15603c880d7946
Sourabh Pathak <63462210+sourabh1428@users.noreply.github.com>
2022-11-02 19:08:40 +0530

    Time: 4 ms (92.71%), Space: 43.8 MB (58.71%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,37 +1,46 @@
 class Solution {
     
-    
-    public void solve(int ind,int[] arr,int k,List<List<Integer>>ans,ArrayList<Integer>ds){
+    public void solve(int ind,int k,int[] arr,List<List<Integer>>ans,ArrayList<Integer>ds)
+    {
         
         if(k==0){
+            
             ans.add(new ArrayList<>(ds));
             return;
+            
         }
         
         for(int i=ind;i<arr.length;i++){
             
             if(i>ind && arr[i]==arr[i-1])continue;
-            
             if(arr[i]>k)break;
             
             ds.add(arr[i]);
             
-            solve(i+1,arr,k-arr[i],ans,ds);
+            solve(i+1,k-arr[i],arr,ans,ds);
+            
             ds.remove(ds.size()-1);
+            
+            
         }
         
         
-    }
     
+        
+        
+    }
     
     
     
     
     
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-       List<List<Integer>>ans=new ArrayList<>();
+    
+        List<List<Integer>>ans=new ArrayList<>();
         Arrays.sort(candidates);
-        solve(0,candidates,target,ans,new ArrayList<>());
+        solve(0,target,candidates,ans,new ArrayList<Integer>());
+        
         return ans;
     }
+    
 }
