5ffddc6c8a083b7ce7cc46faff25307a605db5c6
Sourabh Pathak <63462210+sourabh1428@users.noreply.github.com>
2022-11-02 17:44:14 +0530

    Time: 3 ms (90.51%), Space: 44.1 MB (82.17%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,23 +1,35 @@
 class Solution {
     
-    private void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
-        if(ind == arr.length) {
-            if(target == 0) {
-                ans.add(new ArrayList<>(ds)); 
-            }
-            return; 
-        }
+    
+    public void solve(int ind,int[] arr, int k,List<List<Integer>>wrap,List<Integer>l1){
         
-        if(arr[ind] <= target) {
-            ds.add(arr[ind]);
-            findCombinations(ind, arr, target - arr[ind], ans, ds); 
-            ds.remove(ds.size() - 1); 
-        }
-        findCombinations(ind + 1, arr, target, ans, ds);
+        if(ind==arr.length)
+        {
+            if(k==0){
+                wrap.add(new ArrayList<>(l1));    
+                }
+            return;
+            }
+        
+        if(arr[ind]<=k){
+            l1.add(arr[ind]);
+            solve(ind,arr,k-arr[ind],wrap,l1);
+            l1.remove(l1.size()-1);
+            
+           }
+        
+        solve(ind+1,arr,k,wrap,l1);
     }
+    
+    
+    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        findCombinations(0, candidates, target, ans, new ArrayList<>()); 
-        return ans; 
+        
+        List<List<Integer>>wrap=new ArrayList<>();
+        solve(0,candidates,target,wrap,new ArrayList<>());
+        
+        return wrap;
+        
+        
     }
 }
