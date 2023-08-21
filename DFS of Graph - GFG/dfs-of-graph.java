e5ffef998d3094790d27149945882190569b6fba
Sourabh Pathak <63462210+sourabh1428@users.noreply.github.com>
2023-08-21 23:33:38 +0530

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer>ans=new ArrayList<>();
+        boolean[] vis=new boolean[V+1];
+        Arrays.fill(vis,true);
+        vis[0]=false;
+        dfs(0,ans,adj,vis);
+        return ans;
+        
+    }
+    public void dfs(int ind,ArrayList<Integer>ans,ArrayList<ArrayList<Integer>>adj,boolean[] vis){
+          
+          vis[ind]=false;
+          ans.add(ind);
+
+            for(Integer ind1:adj.get(ind)){
+                if(vis[ind1]!=false){
+                dfs(ind1,ans,adj,vis);
+                }
+                
+            }
+            
+        }
+    }
