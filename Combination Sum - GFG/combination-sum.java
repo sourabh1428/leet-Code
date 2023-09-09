c33d95d448c7450661b764923376d5de270c5c3a
Sourabh Pathak <63462210+sourabh1428@users.noreply.github.com>
2023-09-09 14:14:37 +0530

    Added solution - LeetHub

---
+++ Combination Sum - GFG/combination-sum.java
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driverclass
+{
+    static ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+    public static void main (String[] args)throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine());
+        
+        while(t-- >0)
+        {
+            String str[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(str[0]);
+            ArrayList<Integer> list = new ArrayList<>();
+            str = br.readLine().trim().split(" ");
+            for(int i = 0; i <n ;i++)
+                list.add(Integer.parseInt(str[i]));
+            str = br.readLine().trim().split(" ");    
+            int sum = Integer.parseInt(str[0]);
+            
+            Solution ob = new Solution();
+            
+            res = ob.combinationSum(list, sum);
+            if (res.size() == 0) {
+    			out.print("Empty");
+    		}
+ 
+    		// Print all combinations stored in res.
+    		for (int i = 0; i < res.size(); i++) {
+    			if (res.size() > 0) {
+    				out.print("(");
+    				List<Integer> ij = res.get(i);
+    				for (int j = 0; j < ij.size(); j++) {
+    				    
+    					out.print(ij.get(j));
+    					if(j < ij.size()-1)
+    					 out.print(" ");
+    				}
+    				out.print(")");
+    			}
+    		}
+    		out.println();
+    		res.clear();
+	    }
+	    out.flush();
+    }
+    
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    //Function to return a list of indexes denoting the required 
+    //combinations whose sum is equal to given number.
+    static ArrayList<ArrayList<Integer>> combinationSum(ArrayList<Integer> A, int B)
+    {
+        // add your code here
+        ArrayList<ArrayList<Integer>>ans=new ArrayList<>();
+
+        HashSet<Integer>set=new HashSet<>(A);
+        A.clear();
+        A.addAll(set);
+        Collections.sort(A);
+        solve(0,ans,new ArrayList<>(),A,B);
+        
+        return ans;
+        
+    }
+    static void solve(int ind,ArrayList<ArrayList<Integer>>ans,ArrayList<Integer>ds,ArrayList<Integer>arr,int x){
+        if(ind>=arr.size()){
+            if(x==0){
+                ans.add(new ArrayList<>(ds));
+              
+            }
+              return;
+        }
+        
+        if(x>=arr.get(ind)){
+            ds.add(arr.get(ind));
+            solve(ind,ans,ds,arr,x-arr.get(ind));
+            ds.remove(ds.size()-1);
+        }
+        solve(ind+1,ans,ds,arr,x);
+        
+        
+    }
+}
