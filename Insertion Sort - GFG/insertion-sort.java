236867eff7264bf201d0f5d3501063051f6df402
Sourabh Pathak <63462210+sourabh1428@users.noreply.github.com>
2023-09-28 23:04:11 +0530

    Added solution - LeetHub

--- Insertion Sort - GFG/insertion-sort.java
+++ Insertion Sort - GFG/insertion-sort.java
@@ -49,17 +49,18 @@ class Solution
       
       
       for(int i=0;i<n-1;i++){
-        
+         boolean flag=true;
         for(int j=0;j<n-i-1;j++){
-            
+           
             if(arr[j]>arr[j+1]){
                 int x=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=x;
+                flag=false;
             }
             
         }    
-          
+          if(flag ==true)break;
       }
       
   }
